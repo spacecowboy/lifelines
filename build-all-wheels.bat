0011bdd7edd5f2d5c52145aefa78b3080de1eee1
Jonas Kalderstam <jonas@kalderstam.se>
2014-06-05 13:08:48 +0200

    Added the build script
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

---
+++ build-all-wheels.bat
@@ -0,0 +1,55 @@
+:: Required: git, Visual Studio 10.0 Express, Windows 7 SDK
+
+:: Save original path so we can restore it
+set BASEPATH=%PATH%
+:: These depends on where you installed Anaconda.
+:: Python2 or Python3 doesn't matter but you need both 32bit and 64bit versions.
+set ANACONDA_BASE=C:\Users\jonas\Anaconda
+set ANACONDA_BASE64=C:\Users\jonas\Anaconda-64
+
+:: Set up your conda environments like this, do it for both 32bit and 64bit.
+:: navigate to the Anaconda\Scripts / Anaconda-64\Scripts directory to avoid setting any Paths
+:: conda create -n py3.4 python=3.4 numpy pip mingw
+:: conda create -n py2.7 python=2.7 numpy pip libpython
+
+:: These depend on what you named your environments above
+set ENV27=envs\py2.7
+set ENV34=envs\py3.4
+
+:: Python 2.7 32bit
+set PATH=%ANACONDA_BASE%\%ENV27%;%ANACONDA_BASE%\%ENV27%\Scripts;%BASEPATH%
+pip install wheel
+pip wheel --no-deps git+https://github.com/CamDavidsonPilon/lifelines.git 
+
+:: Python 2.7 64bit
+:: You need to modify numpy in this environment's directory!
+:: Open %ANACONDA_BASE64%\%ENV27%\Lib\site-packages\numpy\distutils\fcompiler\gnu.py
+:: Search for 'raise NotImplementedError("Only MS compiler supported with gfortran on win64")', around line 331
+:: replace with: pass
+set PATH=%ANACONDA_BASE64%\%ENV27%;%ANACONDA_BASE64%\%ENV27%\Scripts;%BASEPATH%
+pip install wheel
+pip wheel --no-deps git+https://github.com/CamDavidsonPilon/lifelines.git 
+
+:: Python3 32bit
+:: Python3 requires Visual Studio 2010 compiler present (Express version is fine)
+set PATH=%BASEPATH%
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+set CPATH=%PATH%
+
+set PATH=%ANACONDA_BASE%\%ENV34%;%ANACONDA_BASE%\%ENV34%\Scripts;%CPATH%
+pip install wheel
+pip wheel --no-deps git+https://github.com/CamDavidsonPilon/lifelines.git 
+
+:: Python3 64bit
+:: Use the SDK to enable x64 compiler since it is not included in express version
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd" /Release /x64
+:: 64-bit cl.exe is here
+set CPATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\x86_amd64;%CPATH%
+
+set PATH=%ANACONDA_BASE64%\%ENV34%;%ANACONDA_BASE64%\%ENV34%\Scripts;%CPATH%
+pip install wheel
+pip wheel --no-deps git+https://github.com/CamDavidsonPilon/lifelines.git 
+
+
+:: Restore path
+set PATH=%BASEPATH%
